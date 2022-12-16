80d5a8dab7eade230a79ef4346ecdee88153c406
ibrahim.karatoprak <113822764+ibrahimkaratoprak@users.noreply.github.com>
2022-12-16 14:39:04 +0300

    Patika_dev_Java_hesap_makinasi

---
+++ Hesap_makinasi.java
@@ -0,0 +1,24 @@
+package PatikaJava;
+import java.util.Scanner;
+public class Hesap_makinasi {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,muzik,tarih;
+        Scanner input=new Scanner(System.in);
+        System.out.print("matematik notunuzu giriniz:");
+        mat=input.nextInt();
+        System.out.print("fizik notunuzu giriniz:");
+        fizik=input.nextInt();
+        System.out.print("kimya notunuzu giriniz:");
+        kimya=input.nextInt();
+        System.out.print("turkce notunuzu giriniz:");
+        turkce=input.nextInt();
+        System.out.print("müzik notunuzu giriniz:");
+        muzik=input.nextInt();
+        System.out.print("tarih notunuzu giriniz:");
+        tarih =input.nextInt();
+        int toplam=(mat+fizik+kimya+turkce+muzik+tarih);
+        double sonuc=toplam/6;
+        System.out.println("ortalama:"+ sonuc);
+
+    }
+}
